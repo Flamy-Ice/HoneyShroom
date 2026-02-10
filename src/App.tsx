c3f9c9cf3c4bf1c39bf07732a4a2ab9b5655af3a
Flamy <jlewandowski4@edu.cdv.pl>
2026-02-10 16:40:58 +0100

    Added VGen logo to Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import titleImg from './assets/Header/HoneyShroomTitle.svg'
 import priceListImg from './assets/Header/PriceList.svg'
 import paperPriceImg from './assets/Header/PaperPrice.svg'
 import blankPaperImg from './assets/Header/BlankPaper.svg'
-import twitterLogo from './assets/Header/Twitterlogo.svg' // Nowy import
+import twitterLogo from './assets/Header/Twitterlogo.svg'
+import vgenLogo from './assets/Header/vgenlogo.svg' // Nowy import
 
 function App() {
   return (
@@ -18,11 +19,18 @@ function App() {
             className="blank-paper-svg" 
           />
 
-          {/* Logo Twittera nałożone na blank-paper */}
+          {/* Logo Twittera */}
           <img 
             src={twitterLogo} 
             alt="Twitter Logo" 
             className="twitter-logo-svg" 
+          />
+
+          {/* NOWY ELEMENT: Logo VGen nałożone na blank-paper */}
+          <img 
+            src={vgenLogo} 
+            alt="VGen Logo" 
+            className="vgen-logo-svg" 
           />
           
           <img 
