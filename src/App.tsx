8a92fe95005e039fc0f7d5dd704080c15fb61de8
Flamy <jlewandowski4@edu.cdv.pl>
2026-02-10 16:27:43 +0100

    Added Basic Images to the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import './App.css'
+import titleImg from './assets/Header/HoneyShroomTitle.svg'
+import priceListImg from './assets/Header/PriceList.svg'
+import paperPriceImg from './assets/Header/PaperPrice.svg'
 
 function App() {
   return (
-    <main>
-    </main>
+    <div className="app-wrapper">
+      <main>
+        <header className="hero-container">
+          {/* Warstwa tła (papier) - pod spodem dzięki z-index: 1 */}
+          <img 
+            src={paperPriceImg} 
+            alt="Paper Background" 
+            className="paper-price-svg" 
+          />
+          
+          {/* Warstwa tekstowa - na wierzchu dzięki z-index: 2 */}
+          <div className="text-content">
+            <img 
+              src={titleImg} 
+              alt="HoneyShroom Title" 
+              className="header-image title-svg" 
+            />
+            <img 
+              src={priceListImg} 
+              alt="Price List" 
+              className="header-image pricelist-svg" 
+            />
+          </div>
+        </header>
+      </main>
+    </div>
   )
 }
 
