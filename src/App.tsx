d7f88fcb770faccd861a5f167b3dc93c31e3a94d
Flamy <jlewandowski4@edu.cdv.pl>
2026-02-14 14:50:37 +0100

    Basic Containers Positioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import './styles/App.css'
+import './styles/header.css';
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <header className="main-header">
+        <div className="orange-container">
+          POMARAŃCZOWY (GÓRA)
+        </div>
+
+        <div className="sides-wrapper">
+          <div className="blue-container">
+            NIEBIESKI (LEWA STRONA)
+          </div>
+          <div className="red-container">
+            CZERWONY (PRAWA STRONA)
+          </div>
+        </div>
+      </header>
+      
+      {/* Gdy tutaj dodasz <section> lub <footer>, header naturalnie się do nich dopasuje */}
+    </div>
+  );
 }
 
-export default App
+export default App;
