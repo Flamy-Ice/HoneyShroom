40a9a2466ea1f750cceb8d161872f665957f86a6
Flamy <jlewandowski4@edu.cdv.pl>
2026-02-17 22:42:42 +0100

    Social Media Icons working as they should

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-// Importy obrazków z Twojej ścieżki: src/assets/header/
 import priceSheet from './assets/header/pricesheet.svg';
 import blankPaper from './assets/header/blankpaper.svg';
+import twitterIcon from './assets/header/twitter.svg';
+import vgenIcon from './assets/header/vgen.svg';
+import blueskyIcon from './assets/header/bluesky.svg';
+import furaffinityIcon from './assets/header/furaffinity.svg';
 
 import './styles/header.css';
 
@@ -14,19 +17,51 @@ function App() {
 
         <div className="sides-wrapper">
           <div className="blue-container">
-            {/* Kontener spinający oba obrazki */}
             <div className="images-wrapper">
-              {/* Używamy zaimportowanych zmiennych w klamrach {} */}
-              <img 
-                src={priceSheet} 
-                className="img-main" 
-                alt="Price Sheet" 
-              />
-              <img 
-                src={blankPaper} 
-                className="img-overlay" 
-                alt="Blank Paper" 
-              />
+              {/* Arkusz cen - WARSTWA ŚRODKOWA */}
+              <img src={priceSheet} className="img-main" alt="Price Sheet" />
+              
+              <div className="img-overlay">
+                {/* Papier - WARSTWA DOLNA */}
+                <img src={blankPaper} className="paper-base-img" alt="Blank Paper" />
+                
+                {/* IKONY JAKO LINKI - WARSTWA GÓRNA */}
+                <a 
+                  href="https://x.com/HoneyShroomArt" 
+                  target="_blank" 
+                  rel="noreferrer" 
+                  className="social-icon icon-twitter"
+                >
+                  <img src={twitterIcon} alt="Twitter" />
+                </a>
+
+                <a 
+                  href="https://vgen.co/HoneyShroom" 
+                  target="_blank" 
+                  rel="noreferrer" 
+                  className="social-icon icon-vgen"
+                >
+                  <img src={vgenIcon} alt="VGen" />
+                </a>
+
+                <a 
+                  href="https://bsky.app/profile/honey-shroom.bsky.social" 
+                  target="_blank" 
+                  rel="noreferrer" 
+                  className="social-icon icon-bluesky"
+                >
+                  <img src={blueskyIcon} alt="BlueSky" />
+                </a>
+
+                <a 
+                  href="https://www.furaffinity.net/user/balladune" 
+                  target="_blank" 
+                  rel="noreferrer" 
+                  className="social-icon icon-furaffinity"
+                >
+                  <img src={furaffinityIcon} alt="FurAffinity" />
+                </a>
+              </div>
             </div>
           </div>
           <div className="red-container">
